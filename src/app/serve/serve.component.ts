48e2bb9e5314649768d07d1d5559044b7b949c58
phat181999 <phat181999@gmail.com>
2022-11-29 14:14:06 +0700

    first commit

---
+++ src/app/serve/serve.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './serve.component.html',
+})
+export class Servecomponent {}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'servers-app',
+  template: 'servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServerComponent {
+  constructor() {}
+
+  ngOnInit() {}
+}
